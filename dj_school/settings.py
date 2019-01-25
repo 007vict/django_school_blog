0c516ee4e38f24fe304c524666f842b1ae2b81f0
007vict <super_vg@bk.ru>
2019-01-25 10:03:52 +0300

    add summernote, req and README second

--- dj_school/settings.py
+++ dj_school/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'django_summernote',
     'news',
 ]
 
@@ -125,3 +125,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- news/admin.py
+++ news/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from news.models import News, Category, Tag
 
-admin.site.register(News)
+class NewsAdmin(SummernoteModelAdmin):
+    summernote_fields = ('text_min', 'text')
+
+admin.site.register(News,NewsAdmin)
 admin.site.register(Category)
 admin.site.register(Tag)
 
